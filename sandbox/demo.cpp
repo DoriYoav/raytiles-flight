baba94d7da79809149aa8ad4cd63efb2a23fb221
Ziv Perry <ziv.perry@zoominfo.com>
2026-05-15 07:45:28 +0300

    refactor: add demo app

---
+++ sandbox/demo.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <string>
+#include <raytiles/raytiles.h>
+#include "fly.h"
+
+int main() {
+    SetTraceLogLevel(LOG_INFO);
+    InitWindow(800, 600, "raytiles");
+
+    raytiles::config conf;
+    raytiles::pool_config pool_conf;
+
+    raytiles::streamer streamer(conf, pool_conf);
+    streamer.set_normals_scale(5.0f);
+
+    Camera3D camera;
+    camera.position = Vector3{3000.0f, 5000.0f, 3000.0f};
+    camera.target = Vector3{10000.0f, 0.0f, 1000.0f};
+    camera.up = Vector3{0.0f, 1.0f, 0.0f};
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    FreeCamera f(camera);
+    streamer.set_fog_color(SKYBLUE);
+    streamer.set_ambient_light(Color{200, 200, 200, 255});
+    float sun = 1.0f;
+
+
+    while (!WindowShouldClose()) {
+        const auto dt = GetFrameTime();
+        f.update(camera, dt);
+
+        streamer.set_sun_direction(Vector3{0.1f, sun, 0.0f});
+        streamer.update(camera);
+
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+
+        BeginMode3D(camera);
+        // draw the world around the camera
+        streamer.draw(camera);
+        streamer.debug_3d(camera);
+        EndMode3D();
+        streamer.debug(camera);
+        EndDrawing();
+
+        if (IsKeyDown(KEY_LEFT_BRACKET)) sun -= dt * 0.5f;
+        if (IsKeyDown(KEY_RIGHT_BRACKET)) sun += dt * 0.5f;
+        sun = std::clamp(sun, -1.0f, 1.0f);
+    }
+
+    CloseWindow();
+    return 0;
+}
