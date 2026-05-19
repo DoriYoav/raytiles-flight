3c74e2a770e55e2c62c606710953826d67e82e40
Ziv Perry <ziv.perry@gmail.com>
2026-05-19 13:18:09 +0300

    refactor: addd model and autopilot to demo app

---
+++ res/readme.md
@@ -0,0 +1,17 @@
+# Resources
+
+## X-wing Starfighter
+
+Used in the demo application.
+
+- Created by [tegnemaskin](https://sketchfab.com/tegnemaskin)
+- Model: [Star Wars T65 X-wing Starfighter](https://sketchfab.com/3d-models/star-wars-t65-x-wing-starfighter-c09ae1a6421748db9d8e8af0d63a93b6)
+- License: [CC Attribution](https://creativecommons.org/licenses/by/4.0/)
+
+## Tie Fighter
+
+Used in the demo application.
+
+- Created by [Mickael Boitte](https://sketchfab.com/boittemike1)
+- Model: [3D T.I.E Fighter - Star Wars model](https://sketchfab.com/3d-models/3d-tie-fighter-star-wars-model-5375de94c2484ab0b2a2bd75aa63c2b4)
+- License: [CC Attribution](https://creativecommons.org/licenses/by/4.0/)
--- sandbox/demo.cpp
+++ sandbox/demo.cpp
@@ -10,7 +10,7 @@ static std::string required_env(const char *name, std::string_view label) {
     throw std::runtime_error(std::format("missing {} token in options or environment variables", label));
 }
 
-Vector3 GetSplinePoint(const Vector3 p0, const Vector3 p1, const Vector3 p2, const Vector3 p3, float t) {
+Vector3 get_spline_point(const Vector3 p0, const Vector3 p1, const Vector3 p2, const Vector3 p3, const float t) {
     const float t2 = t * t;
     const float t3 = t2 * t;
 
@@ -26,16 +26,24 @@ Vector3 GetSplinePoint(const Vector3 p0, const Vector3 p1, const Vector3 p2, con
 }
 
 int main() {
-    // std::vector<Vector3> path = {
-    //     {0.0f, 5000.0f, 0.0f},
-    //     {500.0f, 5000.0f, 0.0f},
-    //     {500.0f, 5000.0f, 500.0f},
-    //     {0.0f, 5000.0f, 500.0f},
-    //     {0.0f, 5000.0f, 500.0f},
-    //     {0.0f, 5000.0f, 500.0f},
-    // };
-    // float flight_progress = 0.0f;
-    // float flight_speed = 0.1f;
+    // Autopilot - Star Ward Scene in the Grand Canyon
+    std::vector<Vector3> path = {
+        {2468.0f, 1339.0f, 380.0f},
+        {1525.0f, 1054.0f, 1230.0f},
+        {509.0f, 963.0f, 2326.0f},
+        {-596.0f, 1012.0f, 2768.0f},
+        {-1493.0f, 1066.0f, 3736.0f},
+        {-2157.0f, 998.0f, 4946.0f},
+        {-2736.0f, 1036.0f, 5688.0f},
+        {-2890.0f, 1121.0f, 6991.0f},
+        {-3367.0f, 1231.0f, 7762.0f},
+        {-13233.0f, 4759.0f, 20554.0f},
+        {-15447.0f, 5597.0f, 23428.0f},
+    };
+    float flight_progress = 0.0f;
+    int step = 0;
+    bool auto_pilot = false;
+
 
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(800, 600, "raytiles");
@@ -58,43 +66,39 @@ int main() {
     // world.anchor_x_tile = 248;
     // world.anchor_z_tile = 160;
     // world.base_zoom_tile_size = 43769;
-    // rendering.height_scale = 1.5f;
-    // rendering.skirt_drop = 1000.0f;
 
     // The Dolomites
     // world.anchor_x_tile = 273;
     // world.anchor_z_tile = 180;
-    // rendering.skirt_drop = 1000.0f;
 
 
     // The Grand Canyon
-    // world.anchor_x_tile = 97;
-    // world.anchor_z_tile = 200;
-    // rendering.skirt_drop = 1000.0f;
+    world.anchor_x_tile = 97;
+    world.anchor_z_tile = 200;
 
     // Norway
     // world.anchor_x_tile = 265;
     // world.anchor_z_tile = 143;
-    // rendering.skirt_drop = 1000.0f;
 
     // Grand Teton
     // world.anchor_x_tile = 98;
     // world.anchor_z_tile = 186;
-    // rendering.skirt_drop = 1000.0f;
 
     // crete
     // world.anchor_x_tile = 292;
     // world.anchor_z_tile = 202;
-    // world.skirt_overlap = {
-    //     {9, 1.02f},
-    //     {10, 1.02f},
-    //     {11, 1.02f},
-    //     {12, 1.02f},
-    //     {13, 1.02f},
-    //     {14, 1.02f},
-    //     {15, 1.02f}
-    // };
-    // rendering.skirt_drop = 1000.0f;
+
+
+    world.skirt_overlap = {
+        {9, 1.01f},
+        {10, 1.01f},
+        {11, 1.01f},
+        {12, 1.01f},
+        {13, 1.01f},
+        {14, 1.01f},
+        {15, 1.01f}
+    };
+    rendering.skirt_drop = 1000.0f;
     // rendering.height_scale = 1.5f;
     // rendering.fog_start = 40000.0f;
 
@@ -112,11 +116,16 @@ int main() {
 
     FreeCamera f(camera);
 
+    // Model x_wing = LoadModel("res/x-wing/scene.gltf");
+    Model x_wing = LoadModel("res/tie/scene.gltf");
+    x_wing.transform = MatrixMultiply(MatrixRotateX(10.0f * DEG2RAD), MatrixRotateY(15.0f * DEG2RAD));
+
     r.set_fog_color(SKYBLUE);
     r.set_ambient_light(Color{200, 200, 200, 255});
     float sun = 1.0f;
     bool wireframe = true;
     bool labels = true;
+    bool crashed = false;
 
     // loading loop
     for (;;) {
@@ -135,18 +144,52 @@ int main() {
         const auto dt = GetFrameTime();
         streamer.update(camera);
 
-        // if (streamer.is_loading()) {
-        //     const auto loading = streamer.get_loading();
-        //     BeginDrawing();
-        //     ClearBackground(BLACK);
-        //     DrawText(TextFormat("Loading... %.1f%%", loading * 100.0f), 10, 30, 20, WHITE);
-        //     EndDrawing();
-        //     continue;
-        // }
+        if (auto_pilot && !crashed) {
+            float step_time = 5.0f;
+            flight_progress += dt / step_time;
+
+            if (flight_progress >= 1.0f) {
+                flight_progress -= 1.0f;
+                step++;
+                if (step >= path.size() - 3) step = 0;
+            }
+            camera.position = get_spline_point(
+                path[step % path.size()],
+                path[(step + 1) % path.size()],
+                path[(step + 2) % path.size()],
+                path[(step + 3) % path.size()],
+                flight_progress
+            );
+
+            // look ahead...
+            camera.target = get_spline_point(
+                path[(step + 1) % path.size()],
+                path[(step + 2) % path.size()],
+                path[(step + 3) % path.size()],
+                path[(step + 4) % path.size()],
+                flight_progress + 0.2f
+            );
+        }
+
+        const auto to_target = camera.target - camera.position;
+        const auto forward = Vector3Normalize(to_target);
+        // const auto right = Vector3Normalize(Vector3CrossProduct(forward, camera.up));
+        // const auto up = Vector3Normalize(Vector3CrossProduct(right, forward));
+
+        // rotate model 90 deg left on xz plane, so it faces forward
+        // const Matrix rot = MatrixRotateY(-PI / 2);
+        float angle = atan2f(forward.x, forward.z) * RAD2DEG;
+        Vector3 rotationAxis = {0.0f, 1.0f, 0.0f};
 
 
-        f.update(camera, dt);
-        r.set_sun_direction(Vector3{0.1f, sun, 0.0f});
+        if (!crashed) {
+            f.update(camera, dt);
+        }
+
+        //r.set_sun_direction(Vector3{0.1f, sun, 0.0f});
+
+        const auto h = streamer.ground_height(camera.position).value_or(0.0f);
+        if (h > camera.position.y) crashed = true;
 
         BeginDrawing();
         ClearBackground(SKYBLUE);
@@ -154,7 +197,12 @@ int main() {
         BeginMode3D(camera);
         // draw the world around the camera
         streamer.draw(camera);
-        if (wireframe) streamer.debug_3d();
+        const Vector3 model_pos = Vector3Add(camera.position, Vector3Scale(forward, 50.0f));
+
+        DrawModelEx(x_wing, model_pos, rotationAxis, angle, {2.0f, 2.0f, 2.0f}, WHITE);
+        if (wireframe) {
+            streamer.debug_3d();
+        }
         EndMode3D();
 
         if (labels) {
@@ -162,8 +210,26 @@ int main() {
             streamer.debug(camera);
         }
 
-        DrawRectangle(5, 550, 600, 40, Fade(BLACK, 0.5f));
-        DrawText("Controls: K to toggle labels, L to toggle wireframe", 10, 560, 20, WHITE);
+        // DrawRectangle(5, 550, 600, 40, Fade(BLACK, 0.5f));
+        // DrawText("Controls: K to toggle labels, L to toggle wireframe", 10, 560, 20, WHITE);
+
+        DrawRectangle(5, 50, 200, 40, Fade(BLACK, 0.5f));
+        DrawText(TextFormat("P %d %d %d",
+                            static_cast<int>(camera.position.x),
+                            static_cast<int>(camera.position.y),
+                            static_cast<int>(camera.position.z)
+                 ), 10, 60, 20, WHITE);
+
+        if (crashed) {
+            DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, 0.5f));
+            DrawText("You crashed! Press R to reset.", 200, 300, 30, WHITE);
+            if (IsKeyPressed(KEY_R)) {
+                camera.position = Vector3{2000.0f, 5000.0f, 2000.0f};
+                camera.target = Vector3{3000.0f, 4750.0f, 3000.0f};
+                // f = FreeCamera(camera);
+                crashed = false;
+            }
+        }
         EndDrawing();
 
         if (IsKeyDown(KEY_LEFT_BRACKET)) sun -= dt * 0.5f;
@@ -172,6 +238,7 @@ int main() {
 
         if (IsKeyPressed(KEY_L)) wireframe = !wireframe;
         if (IsKeyPressed(KEY_K)) labels = !labels;
+        if (IsKeyPressed(KEY_P)) auto_pilot = !auto_pilot;
     }
 
     CloseWindow();
